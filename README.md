b71ffd95ba6aac6c88c63856dfba2029ea7110a9
Jonatan Ivanov <jonatan.ivanov@gmail.com>
2018-04-07 16:26:08 -0700

    Recreating using lazybones template

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 # truststore-checker
 
-A simple cli to check your truststore over HTTPS.  
-Example: `java -jar build/libs/truststore-checker.jar --trustStore $JAVA_HOME/jre/lib/security/cacerts --trustStorePassword changeit --url https://httpbin.org/headers`
+A simple cli to check your truststore over HTTPS.
+
+## Run as a script
+
+`./src/main/groovy/truststore-checker.groovy --trustStore $JAVA_HOME/jre/lib/security/cacerts --trustStorePassword changeit --url https://httpbin.org/headers --verbose`
+
+## Build and run as a jar
+
+`./gradlew clean build`
+`java -jar build/libs/truststore-checker.jar --trustStore $JAVA_HOME/jre/lib/security/cacerts --trustStorePassword changeit --url https://httpbin.org/headers --verbose`
---
+++ src/main/groovy/truststore-checker.groovy
@@ -0,0 +1,27 @@
+#! /usr/bin/env groovy
+
+import javax.net.ssl.HttpsURLConnection
+
+/**
+ * @author Jonatan Ivanov
+ */
+CliBuilder cli = new CliBuilder(usage: "${this.class.simpleName}.groovy <options>", width: 250)
+cli.with {
+    h longOpt: 'help', 'Show usage information'
+    _ longOpt: 'url', args: 1, required: true, argName: 'url', ''
+    _ longOpt: 'trustStore', args: 1, required: true, argName: 'trustStore', ''
+    _ longOpt: 'trustStorePassword', args: 1, required: true, argName: 'trustStorePassword', ''
+    _ longOpt: 'verbose', ''
+}
+
+OptionAccessor options = cli.parse(args)
+if (options == null || options.h) {
+    System.exit(1)
+}
+
+if (options.verbose) System.setProperty('javax.net.debug', 'all')
+System.setProperty('javax.net.ssl.trustStore', options.trustStore)
+System.setProperty('javax.net.ssl.trustStorePassword', options.trustStorePassword)
+HttpsURLConnection.setDefaultHostnameVerifier({ hostname, session -> true })
+
+println options.url.toURL().text
